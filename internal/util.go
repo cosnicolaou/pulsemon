2d9fa8482b346f01a8665d2aaa0558f3fe588613
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-01-30 19:37:17 -0800

    .

--- internal/util.go
+++ internal/util.go
@@ -5,8 +5,8 @@ import "time"
 // UntilHHMM returns the duration until the specified time (in 24 hours and
 // minutes) will be reached.
 func UntilHHMM(hhmm time.Time) time.Duration {
-	now := time.Now()
-	then := time.Date(now.Year(), now.Month(), now.Day(), hhmm.Hour(), hhmm.Minute(), 0, 0, time.Local)
+	now := time.Now().UTC()
+	then := time.Date(now.Year(), now.Month(), now.Day(), hhmm.Hour(), hhmm.Minute(), 0, 0, time.UTC)
 	until := then.Sub(now)
 	if until < 0 {
 		until += 24 * time.Hour
