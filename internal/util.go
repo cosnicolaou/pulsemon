67a6ab23ec31da8b005e14e3fb5687023e9275fa
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-11-13 08:40:46 -0800

    use time.UTC rather time.Now, update dependencies. (#5)

--- internal/util.go
+++ internal/util.go
@@ -5,11 +5,15 @@ import "time"
 // UntilHHMM returns the duration until the specified time (in 24 hours and
 // minutes) will be reached.
 func UntilHHMM(hhmm time.Time) time.Duration {
-	now := time.Now()
-	then := time.Date(now.Year(), now.Month(), now.Day(), hhmm.Hour(), hhmm.Minute(), 0, 0, time.Local)
+	now := time.Now().UTC()
+	then := time.Date(now.Year(), now.Month(), now.Day(), hhmm.UTC().Hour(), hhmm.UTC().Minute(), 0, 0, time.UTC)
 	until := then.Sub(now)
 	if until < 0 {
 		until += 24 * time.Hour
 	}
 	return until
+}
+
+func HHMM(hhmm time.Time) string {
+	return hhmm.Format("15:04")
 }
